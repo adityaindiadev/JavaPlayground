13c6157170b6688214cc38fc484864437757b707
adityaindiadev <aditya.india.dev@gmail.com>
2023-04-27 12:20:15 +0530

    SolidRombusCompleted

--- PatternsAdvance/SolidRhombus.java
+++ PatternsAdvance/SolidRhombus.java
@@ -2,6 +2,50 @@ package PatternsAdvance;
 
 public class SolidRhombus {
 
+
+    /*
+
+    * * * * 
+   * * * * 
+  * * * * 
+ * * * * 
+* * * * 
+adityagupta
+
+     */
+
+    public static void printSolidRhombus(int line) {
+
+
+        for (int outer = 1; outer <= line; outer++) {
+            
+            for (int inner = 1; inner <= (line - outer) + 1; inner++) {
+                
+
+                System.out.print(" ");
+               
+
+            }
+
+            for (int inner = 1; inner <= line; inner++) {
+                
+
+                System.out.print("* ");
+               
+
+            }
+
+            System.out.println();
+
+        }
+        
+    }
+
     
+    public static void main(String[] args) {
+        
+        printSolidRhombus(4);
+
+    }
 
 }
