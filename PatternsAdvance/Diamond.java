17b38f0b1b77ec6271a3a5564d4cded711078d72
adityaindiadev <aditya.india.dev@gmail.com>
2023-04-27 15:24:46 +0530

    Diamond.java Completed

--- PatternsAdvance/Diamond.java
+++ PatternsAdvance/Diamond.java
@@ -24,40 +24,52 @@ public class Diamond {
     public static void printDiamond(int line) {
 
 
-        for (int outer = 1; outer <= line; outer++) {
-            
-            for (int inner = 1; inner <= (line - outer) + 1; inner++) {
-                
+            for (int outer = 1; outer <= line; outer++) {
 
-                System.out.print(" ");
-               
+                for (int spaces = 1; spaces <= (line - outer); spaces++) {
+                    
+                    System.out.print("  ");
 
-            }
-
-            for (int inner = 1; inner <= line; inner++) {
-                
-
-                if (outer == 1 || outer == line || inner == 1 || inner == line) {
-                    System.out.print( "* ");
                 }
 
-                else{
-                    System.out.print(  "  ");
-                }
-               
+                for (int star = 1; star <= ((2*outer) - 1); star++) {
+                    
+                    System.out.print("* ");
 
+                }
+
+                System.out.println();
+                
+                
             }
 
-            System.out.println();
+            for (int outer = line-1; outer >= 1; outer--) {
+
+                for (int spaces = 1; spaces <= (line - outer); spaces++) {
+                    
+                    System.out.print("  ");
+
+                }
+
+                for (int star = 1; star <= ((2*outer) - 1); star++) {
+                    
+                    System.out.print("* ");
+
+                }
+
+                System.out.println();
+                
+                
+            }
 
         }
         
-    }
+    
 
     
     public static void main(String[] args) {
         
-        printDiamond(4);
+        printDiamond(5);
 
     }
 
