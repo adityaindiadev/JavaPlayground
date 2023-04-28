9c2f4b8ec50b52232032ecd140becfcbaf20b575
adityaindiadev <aditya.india.dev@gmail.com>
2023-04-28 18:39:01 +0530

    PairsInArray.java

---
+++ PatternsAdvance/Arrays/PairsInArray.java
@@ -0,0 +1,35 @@
+package PatternsAdvance.Arrays;
+
+public class PairsInArray {
+    
+    public static void printPairsInArray(int array[]) {
+        
+        
+        for (int i = 0; i < array.length; i++) {
+            
+           for (int j = i+1; j < array.length; j++) {
+            
+                System.out.print(" ( " + array[i] + ", " + array[j] + ") ");
+
+           }
+
+        }
+
+      
+        
+
+    }
+    
+
+
+    public static void main(String[] args) {
+        
+        int arr[] = {2,4,6,8,10};
+
+        printPairsInArray(arr);
+
+        
+
+    }
+
+}
