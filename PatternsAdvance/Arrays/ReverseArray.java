9ac7052bf5ea913eed554c7d8f6071157dc2c5a1
adityaindiadev <aditya.india.dev@gmail.com>
2023-04-28 15:28:47 +0530

    ReverseArray

---
+++ PatternsAdvance/Arrays/ReverseArray.java
@@ -0,0 +1,51 @@
+package PatternsAdvance.Arrays;
+
+public class ReverseArray {
+    
+    public static void reverseAnArray(int arr[]) {
+        
+        
+        // int start = 0;
+        // int end = arr.length-1;
+
+
+        // while (start < end) {
+
+        //     int temp = arr[start];
+        //     arr[start] = arr[end];
+        //     arr[end] = temp;
+
+        //     start++;
+        //     end--;
+            
+        // }
+
+        for (int start = 0, end = arr.length-1; start < arr.length; start++, end--) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {2,3,4,5,6,7,9};
+
+        reverseAnArray(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            System.out.print(arr[i] + " ");
+
+        }
+        
+
+
+    }
+
+}
+
