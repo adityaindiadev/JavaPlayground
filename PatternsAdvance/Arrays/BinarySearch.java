4610cbeed3a70eb88f2875269bbf0ce21bf776d9
adityaindiadev <aditya.india.dev@gmail.com>
2023-04-28 15:05:44 +0530

    LargestInArray

---
+++ PatternsAdvance/Arrays/BinarySearch.java
@@ -0,0 +1,26 @@
+package PatternsAdvance.Arrays;
+
+public class BinarySearch {
+    
+    public static void findItemUsingBinarySearch(int array[], int itemToSearch) {
+
+
+
+       
+        
+
+    }
+    
+
+
+    public static void main(String[] args) {
+        
+        int arr[] = {20,30,40,50,60,70};
+
+        findItemUsingBinarySearch(arr, 70);
+
+        
+
+    }
+
+}
