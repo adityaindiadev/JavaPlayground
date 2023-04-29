6f7d6866fc6e0ef7565cd16c241ca7c2d1ee0a4f
unknown <aditya.india.dev@gmail.com>
2023-04-29 20:22:58 +0530

    SubArray and its Max

--- Arrays/MaxSubArraySum.java
+++ Arrays/MaxSubArraySum.java
@@ -1,35 +1,41 @@
 package Arrays;
 
-public class SubArrays {
+import java.util.*;
 
-    public static void printSubArrays(int array[]) {
+public class MaxSubArraySum {
+
+    public static void printMaxSubArraySum(int array[]) {
 
         int totalSubarrays = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
         for (int first = 0; first < array.length; first++) {
 
             for (int second = first; second < array.length; second++) {
 
-                System.out.print("[");
+                // System.out.print("[");
                 int sumOfSubArray = 0;
 
                 for (int third = first; third <= second; third++) {
 
                     sumOfSubArray = sumOfSubArray + array[third];
 
-                    System.out.print(array[third]);
+                    // System.out.print(array[third]);
 
-                    if (third != second) {
-                        System.out.print(", ");
-                    }
+                    // if (third != second) {
+                    // System.out.print(", ");
+                    // }
 
                 }
 
-                
+                if (sumOfSubArray > max) {
+                    max = sumOfSubArray;
+                }
 
                 totalSubarrays++;
 
-                System.out.print("]  ");
+                // System.out.print("] ");
 
                 System.out.print("sumOfSubArray: " + sumOfSubArray + " |  ");
 
@@ -37,6 +43,7 @@ public class SubArrays {
             System.out.println();
 
         }
+        System.out.println("Max Sum of Sub Array: " + max);
         System.out.println("Total SubArrays: " + totalSubarrays);
 
     }
@@ -45,7 +52,7 @@ public class SubArrays {
 
         int arr[] = { 2, 4, 3, 6 };
 
-        printSubArrays(arr);
+        printMaxSubArraySum(arr);
 
     }
 
