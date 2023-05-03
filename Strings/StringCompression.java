5890c2ba795c429d2d4775c3f1850c9855b0804a
unknown <aditya.india.dev@gmail.com>
2023-05-03 22:09:44 +0530

    Strings/StringCompression.java

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -13,17 +13,27 @@ public class StringCompression {
         for (int i = 1; i < str.length(); i++) {
             
             if (str.charAt(i) == ch) {
-                tempStr.append(str.charAt(i));
+                // tempStr.append(str.charAt(i));
+                // tempStr.append(str.charAt(i));
                 count++;
                 
             }
 
             else{
+                
+                tempStr.append(ch);
                 tempStr.append(count.toString());
                 count = 1;
                 // tempStr.append(str.charAt(i));
                 ch = str.charAt(i);
             }
+            if (i == str.length()-1) {
+                tempStr.append(ch);
+                tempStr.append(count.toString());
+
+
+            }
+            
 
             
             
@@ -36,7 +46,7 @@ public class StringCompression {
 
     public static void main(String[] args) {
         
-        StringBuilder str = new StringBuilder("aabbcdd");
+        StringBuilder str = new StringBuilder("aaabbcdd");
 
         compressString(str);
 
