146c4ffc3c64221f7df7199d26a37a22c7ee596f
adityaindiadev <aditya.india.dev@gmail.com>
2023-05-03 19:55:34 +0530

    StringCompression.java

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -2,9 +2,43 @@ package Strings;
 
 public class StringCompression {
     
+    public static void compressString(StringBuilder str) {
 
+        Integer count = 1;
+
+        StringBuilder tempStr = new StringBuilder("");
+
+        Character ch = str.charAt(0);
+        
+        for (int i = 1; i < str.length(); i++) {
+            
+            if (str.charAt(i) == ch) {
+                tempStr.append(str.charAt(i));
+                count++;
+                
+            }
+
+            else{
+                tempStr.append(count.toString());
+                count = 1;
+                // tempStr.append(str.charAt(i));
+                ch = str.charAt(i);
+            }
+
+            
+            
+
+        }
+
+        System.out.println(tempStr);
+
+    }
 
     public static void main(String[] args) {
         
+        StringBuilder str = new StringBuilder("aabbcdd");
+
+        compressString(str);
+
     }
 }
