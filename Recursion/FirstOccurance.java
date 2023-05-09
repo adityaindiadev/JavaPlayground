4924b5bb9a1b593b0dfaa290758c3ca19e5586ba
unknown <aditya.india.dev@gmail.com>
2023-05-10 02:18:00 +0530

    FirstOccurance

---
+++ Recursion/FirstOccurance.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class FirstOccurance {
+    
+
+    public static void checkFirstOccurance() {
+        
+        
+
+    }
+
+
+    public static void main(String[] args) {
+        
+
+
+
+    }
+}
