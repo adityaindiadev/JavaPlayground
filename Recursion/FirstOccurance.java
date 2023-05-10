448b1bb83de4308bd48798641662c3248e596e52
adityaindiadev <aditya.india.dev@gmail.com>
2023-05-10 14:07:58 +0530

    SecondOccurance.java

--- Recursion/FirstOccurance.java
+++ Recursion/FirstOccurance.java
@@ -3,7 +3,20 @@ package Recursion;
 public class FirstOccurance {
     
 
-    public static void checkFirstOccurance() {
+    public static int checkFirstOccurance(int array[], int key, int index) {
+        
+        if (index == array.length) {
+            return -1;
+        }
+        
+        if (array[index] == key) {
+            
+            return index;
+
+        }
+
+
+        return checkFirstOccurance(array, key, index+1);
         
         
 
@@ -12,7 +25,10 @@ public class FirstOccurance {
 
     public static void main(String[] args) {
         
+        int arr[] = { 1, 3, 4, 5, 6 };
 
+        System.out.println(checkFirstOccurance(arr, 5, 0));
+        
 
 
     }
